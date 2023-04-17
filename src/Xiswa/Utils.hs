9ff5940fa2a1ad683e027a22247d6907b60aa5ab
Hizbullah Abdul Aziz Jabbar <archbung@gmail.com>
2023-04-17 11:45:15 +0700

    Simplify

--- src/Xiswa/Utils.hs
+++ src/Xiswa/Utils.hs
@@ -1,7 +1,28 @@
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE LambdaCase            #-}
 module Xiswa.Utils (
-    module Xiswa.Utils.Helper
-  , module Xiswa.Utils.Class
+    Has(..)
+  , grab
+  , camelToSnakeCase
   ) where
 
-import Xiswa.Utils.Helper
-import Xiswa.Utils.Class
+import Control.Monad.Reader
+import Data.Char (isUpper, toLower)
+
+class Has field env where
+  obtain :: env -> field
+
+grab :: forall field env m . (MonadReader env m, Has field env) => m field
+grab = asks $ obtain @field
+{-# INLINE grab #-}
+
+camelToSnakeCase :: String -> String -> String
+camelToSnakeCase = 
+  let toSnakeCase' =
+        foldr (\c z -> if isUpper c then '_':toLower c:z else c:z) []
+   in \case
+        []   -> toSnakeCase'
+        pref -> tail . toSnakeCase' . drop (length pref)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,18 +1,14 @@
 module Main (main) where
 
 import Test.Hspec
-
-import Xiswa.Utils.Helper
+import Xiswa.Utils
 
 spec :: Spec
 spec = do
-  describe "camelToSnakeWithPref" $ do
-    it "correctly handles prefix" $ do
-      camelToSnakeWithPref "somePrefix" "somePrefixThatIsLong" `shouldBe` "that_is_long"
-
-  describe "camelToSnake" $ do
-    it "correctly converts to snake case" $ do
-      camelToSnake "aCamelCaseName" `shouldBe` "a_camel_case_name"
+  describe "camelToSnakeCase" $ do
+    it "correctly handles prefixes" $ do
+      camelToSnakeCase "somePrefix" "somePrefixThatIsLong" `shouldBe` "that_is_long"
+      camelToSnakeCase ""           "aCamelCaseName"       `shouldBe` "a_camel_case_name"
 
 main :: IO ()
 main = hspec spec
